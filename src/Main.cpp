e0373fba340a2eed859500eae55a9a802e387c36
IceBirdCiel <cceline74@hotmail.fr>
2022-01-20 09:42:11 +0100

    Fixed gitignore

---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/video/tracking.hpp>
+
+
+int main()
+{
+    std::cout << "Close Project" << std::endl;
+}
