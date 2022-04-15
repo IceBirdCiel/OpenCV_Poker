9c49120d04870f425e2bfda73e504f8c3f1d7ed2
Quentin <pamartqu@gmail.com>
2022-04-15 18:00:14 +0200

    namespace + .h for main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
+#include "Main.h"
 
 #include <opencv2/opencv.hpp>
 
 #include "DataSetCards.h"
 #include "MatchCards.h"
-#include "SolutionBResult.h"
 
-SolutionBResult compute(const cv::Mat& testImage)
-{
-    SolutionBResult result;
+namespace solB {
+    SolutionBResult compute(const cv::Mat &testImage) {
+        SolutionBResult result;
 
-    DataSetCards set;
-    cv::Mat im;
-    im = cv::imread("resources/cards.jfif");
-    set.CreateData(im);
+        DataSetCards set;
+        cv::Mat im;
+        im = cv::imread("resources/cards.jfif");
+        set.CreateData(im);
 
-    MatchCards scene;
-    //cv::Mat test = cv::imread("resources/tables_poker/img.png");
-    return scene.Match(testImage, set.data);
-    //std::cout << "Close Project" << std::endl;
+        MatchCards scene;
+        //cv::Mat test = cv::imread("resources/tables_poker/img.png");
+        return scene.Match(testImage, set.data);
+        //std::cout << "Close Project" << std::endl;
+    }
 }
---
+++ src/Main.h
@@ -0,0 +1,9 @@
+// Created by Quentin Pamart on 15/04/2022.
+
+#pragma once
+
+#include "SolutionBResult.h"
+
+namespace solB {
+    SolutionBResult compute(const cv::Mat &testImage);
+}
